27211d26f93df2b0b2562b04397e7587bbb72b25
siddiq2772 <siddiqmohd2772@gmail.com>
2026-08-26 19:47:00 +0530

    first buikd

---
+++ src/api-impl/android/media/AudioDeviceInfo.java
@@ -0,0 +1,12 @@
+package android.media;
+
+public class AudioDeviceInfo {
+	public static final int TYPE_BUILTIN_EARPIECE = 1;
+	public static final int TYPE_BUILTIN_SPEAKER = 2;
+	public static final int TYPE_WIRED_HEADSET = 3;
+	public static final int TYPE_WIRED_HEADPHONES = 4;
+	public static final int TYPE_BLUETOOTH_SCO = 7;
+	public static final int TYPE_BLUETOOTH_A2DP = 8;
+
+	AudioDeviceInfo() {}
+}
---
+++ src/api-impl/android/text/style/StrikethroughSpan.java
@@ -0,0 +1,8 @@
+package android.text.style;
+
+import android.text.ParcelableSpan;
+
+public class StrikethroughSpan extends CharacterStyle implements ParcelableSpan {
+	public StrikethroughSpan() {
+	}
+}
